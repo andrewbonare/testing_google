c83898698d8a394492f11672a39c6d543b1930c4
= <=>
2024-08-31 11:24:58 -0400

    added in tests for class Discount after having sanity tested as well

--- testgoogle.cpp
+++ testgoogle.cpp
@@ -12,12 +12,20 @@ std::string rever(std::string input){
 struct item{
   std::string name;
   float price;
+
+    // Overload the == operator for item comparison
+  bool operator==(const item& other) const {
+    return name == other.name && price == other.price;
+  }
 };
+
+
 
 class ShoppingCart{
  private:
-std::vector<item> basket;
+
  public:
+ std::vector<item> basket;
  void addItem(item a){basket.push_back(a);}
   int findItem(item a){
     for(int i = 0; i < basket.size(); i++){
@@ -26,7 +34,7 @@ std::vector<item> basket;
       }
     }
   }
-  void removeItem(item a){basket.erase(findItem(name));}
+  void removeItem(item a){basket.erase(basket.begin() + findItem(a));}
  float total(){
    float t;
    if(basket.empty()){
@@ -52,15 +60,19 @@ public:
   double getDiscountAmount(){return discountAmount;}
   bool isPercentageDiscount(){return isPercentage;}
   void applyDiscount(ShoppingCart& cart){
-    if(ispercentage){
+    if(isPercentage){
       float s; // for subtrahend
       for(int i = 0; i < cart.basket.size(); i++){
-	s = cart.basket[i].price *  discountAmount;
-	basket[i] -= s;
+	s = cart.basket[i].price *  (discountAmount / 100);
+	cart.basket[i].price -= s;
+	//std::cout << "SANITY CHECK RAAAAH\n";
+	
       }
       
     } else{
-      basket[i] -= discountAmount;
+       for(int i = 0; i < cart.basket.size(); i++){
+	 cart.basket[i].price -= discountAmount;
+       }
     }
   }
   
@@ -85,8 +97,39 @@ TEST(Test, Integration){
   target.addItem(shirt);
   EXPECT_EQ(target.total(), 30);
   
-
 }
+
+
+TEST(Test, DiscountPercent){ //doesn't really fit the above naming scheme
+    ShoppingCart target;
+    item shirt;
+    shirt.name = "shirt";
+    shirt.price = 10;
+    target.addItem(shirt);
+    Discount fiftyOff(50, true);
+
+    EXPECT_EQ(fiftyOff.getDiscountAmount(), 50);
+    EXPECT_EQ(fiftyOff.isPercentageDiscount(), true);
+    fiftyOff.applyDiscount(target);
+    EXPECT_EQ(target.total(), 5);
+}
+
+TEST(Test, DiscountFlatAmount){
+    ShoppingCart target;
+    item shirt;
+    shirt.name = "shirt";
+    shirt.price = 10;
+    target.addItem(shirt);
+    Discount fiveOff(5, false);
+
+    EXPECT_EQ(fiveOff.getDiscountAmount(), 5);
+    EXPECT_EQ(fiveOff.isPercentageDiscount(), false);
+    fiveOff.applyDiscount(target);
+    EXPECT_EQ(target.total(), 5);
+}
+
+
+    
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
